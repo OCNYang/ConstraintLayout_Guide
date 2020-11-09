71da3ce72cae543da5267788e112ba29e85cb496
OCN Yang <ocnyang@gmail.com>
2020-11-09 18:17:37 +0800

    add basics demo;

---
+++ app/src/main/java/com/ocnyang/constraintlayout_guide/basics/ConstraintLayoutBasicsActivity.kt
@@ -0,0 +1,12 @@
+package com.ocnyang.constraintlayout_guide.basics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ocnyang.constraintlayout_guide.R
+
+class ConstraintLayoutBasicsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout_basics)
+    }
+}
